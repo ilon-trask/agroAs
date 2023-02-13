68bb6b3d855b8dbbe89404b0b8028ef7592efb9a
ilon-trask <misha007zyt@gmail.com>
2023-02-13 16:19:48 +0200

    .

--- tRPC serv/controllers/TechCartService.ts
+++ tRPC serv/controllers/TechCartService.ts
@@ -211,6 +211,10 @@ export async function getCart(userId: string | undefined) {
         const [gradeTractor] = Grade.filter((el) => el.id == Tractor.gradeId);
         const [gradeMachine] = Grade.filter((el) => el.id == machine.gradeId);
         const pricePerHourPersonnel = Math.round(cart?.salary / 176);
+        const rareOfProduction = Math.round(
+          (machine.widthOfCapture * (aggregateData.fuelConsumption * 1000)) /
+            10000
+        );
         const costFuel = Math.round(
           (+aggregateData.fuelConsumption * +cart.priceDiesel) /
             Math.round(
@@ -232,12 +236,12 @@ export async function getCart(userId: string | undefined) {
             )
         );
         const costMachineWork = Math.round(
-          pricePerHourPersonnel *
+          (pricePerHourPersonnel / rareOfProduction) *
             (Tractor.numberOfPersonnel ?? 0) *
             gradeTractor?.coefficient!
         );
         const costHandWork = Math.round(
-          pricePerHourPersonnel *
+          (pricePerHourPersonnel / rareOfProduction) *
             (machine.numberOfServicePersonnel ?? 0) *
             gradeMachine?.coefficient!
         );
