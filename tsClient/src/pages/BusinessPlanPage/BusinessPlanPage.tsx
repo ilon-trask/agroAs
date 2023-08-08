8d4bc117f032d2b6eecf76b6f60a0d553937ab47
ilon-trask <misha007zyt@gmail.com>
2023-08-08 14:14:47 +0300

    test

--- tsClient/src/pages/BusinessPlanPage/BusinessPlanPage.tsx
+++ tsClient/src/pages/BusinessPlanPage/BusinessPlanPage.tsx
@@ -46,6 +46,7 @@ import SaleBusTable from "./modules/SaleBusTable";
 import getStartAndEndBusinessPlan from "src/shared/funcs/getStartAndEndBusinessPlan";
 import LandBusTable from "./modules/LandBusTable";
 import PlanYieldBusTable from "./modules/PlanYieldBusTable";
+import als from "src/modules/AgreeCartsTable";
 export function getMonthAmountFromBusinessPlan(
   dateStart: string,
   i: number,
