bbdb66766383095834d7b0eb7ea6d6073f9670eb
ilon-trask <misha007zyt@gmail.com>
2023-08-11 18:01:10 +0300

    before player

--- tsClient/src/pages/MainPage.tsx
+++ tsClient/src/pages/MainPage.tsx
@@ -9,7 +9,6 @@ import {
   Tab,
   TabPanel,
   Button,
-  Heading,
 } from "@chakra-ui/react";
 import MainTableItem from "../components/MainTableItem";
 import { Context } from "../main";
@@ -18,89 +17,96 @@ import SkeletonCart from "../components/SkeletonCart";
 import { useNavigate } from "react-router-dom";
 import { HOW_ROUTER } from "../utils/consts";
 import MainTableItemJustification from "../components/MainTableItemJustification";
+import MyHeading from "src/ui/MyHeading";
 
-function MainPage() {
-  const { map, TEJ } = useContext(Context);
+function HeroSection() {
   const navigate = useNavigate();
-  const windW = window.innerWidth;
-
   return (
-    <Box>
-      <Tabs
-        orientation={windW < 770 ? "horizontal" : "vertical"}
-        w={"100vw"}
-        variant="soft-rounded"
+    <>
+      <Box borderRadius={"20px"}>
+        <Image borderRadius={"20px"} src="../../logo.jpg" alt="логотип" />
+      </Box>
+      <Box
+        borderRadius={"20px"}
+        backgroundImage={"../../title_bg.jpg"}
+        backgroundRepeat={"no-repeat"}
+        backgroundSize={"cover"}
+      >
+        <Box textAlign={"center"} fontWeight={"bold"} fontSize={24}>
+          <Text color={"#20401e"}>AgroDataBase</Text>
+          <Text fontSize={"20px"}>
+            Онлайн - сервіс для планування,
+            <br /> обліку та аналізу витрат фермерського господарства
+          </Text>
+          <Button
+            bg={"rgba( 93, 160, 93, 0.55 )"}
+            borderRadius={100}
+            size={"lg"}
+            fontSize={"20px"}
+            h={"min-content"}
+            py={2}
+            onClick={() => navigate(HOW_ROUTER)}
+          >
+            Як це працює?
+          </Button>
+        </Box>
+      </Box>
+    </>
+  );
+}
+function MapScreen() {
+  const { map } = useContext(Context);
+
+  const windW = window.innerWidth;
+  return (
+    <Tabs
+      orientation={windW < 770 ? "horizontal" : "vertical"}
+      w={"100vw"}
+      variant="soft-rounded"
+    >
+      <Box
+        display={"grid"}
+        gridTemplateColumns={["1fr", "1fr", "1fr 3fr"]}
+        gridColumnGap={"15px"}
+        gridGap={"15px"}
+        maxW={"1300px"}
+        mx={"auto"}
+        mt={"15px"}
       >
         <Box
-          display={"grid"}
-          gridTemplateColumns={["1fr", "1fr", "1fr 3fr"]}
-          gridColumnGap={"15px"}
-          gridGap={"15px"}
-          maxW={"1300px"}
-          mx={"auto"}
-          mt={"15px"}
+          p={"15px"}
+          overflowX={windW < 770 ? "scroll" : undefined}
+          maxW={"100vw"}
         >
-          <Box borderRadius={"20px"}>
-            <Image borderRadius={"20px"} src="../../logo.jpg" alt="логотип" />
-          </Box>
-          <Box
-            borderRadius={"20px"}
-            backgroundImage={"../../title_bg.jpg"}
-            backgroundRepeat={"no-repeat"}
-            backgroundSize={"cover"}
-          >
-            <Box textAlign={"center"} fontWeight={"bold"} fontSize={24}>
-              <Text color={"#20401e"}>AgroDataBase</Text>
-              <Text fontSize={"20px"}>
-                Онлайн - сервіс для планування,
-                <br /> обліку та аналізу витрат фермерського господарства
-              </Text>
-              <Button
-                bg={"rgba( 93, 160, 93, 0.55 )"}
-                borderRadius={100}
-                size={"lg"}
-                fontSize={"20px"}
-                h={"min-content"}
-                py={2}
-                onClick={() => navigate(HOW_ROUTER)}
+          {/* <Text mt={"10px"} fontWeight={"bold"} fontSize={"20px"}>
+        КУЛЬТУРИ
+         </Text> */}
+          <Box>
+            <TabList gap={2}>
+              <Tab
+                display={"block"}
+                textAlign={"left"}
+                _selected={{ bg: "rgba( 93, 160, 93, 0.55 )" }}
               >
-                Як це працює?
-              </Button>
-            </Box>
-          </Box>
-
-          <Box
-            p={"15px"}
-            overflowX={windW < 770 ? "scroll" : undefined}
-            maxW={"100vw"}
-          >
-            {/* <Text mt={"10px"} fontWeight={"bold"} fontSize={"20px"}>
-            КУЛЬТУРИ
-             </Text> */}
-            <Box>
-              <TabList gap={2}>
+                КУЛЬТУРИ
+              </Tab>
+              {map.cultural.map((el) => (
                 <Tab
                   display={"block"}
                   textAlign={"left"}
                   _selected={{ bg: "rgba( 93, 160, 93, 0.55 )" }}
                 >
-                  КУЛЬТУРИ
+                  {el.nameCulture}
                 </Tab>
-                {map.cultural.map((el) => (
-                  <Tab
-                    display={"block"}
-                    textAlign={"left"}
-                    _selected={{ bg: "rgba( 93, 160, 93, 0.55 )" }}
-                  >
-                    {el.nameCulture}
-                  </Tab>
-                ))}
-              </TabList>
-            </Box>
+              ))}
+            </TabList>
           </Box>
+        </Box>
 
-          <TabPanels>
-            <TabPanel>
+        <TabPanels>
+          <TabPanel>
+            <Box>
+              <MyHeading mb={4}>Розрахунок прямих витрат</MyHeading>
               <Box
                 display={"grid"}
                 gridTemplateColumns={[
@@ -120,9 +126,12 @@ function MainPage() {
                       <MainTableItem e={e} key={e.id} />
                     )) || <Text>Немає жодної карти</Text>}
               </Box>
-            </TabPanel>
-            {map.cultural.map((el) => (
-              <TabPanel>
+            </Box>
+          </TabPanel>
+          {map.cultural.map((el) => (
+            <TabPanel>
+              <Box>
+                <MyHeading mb={4}>Розрахунок прямих витрат</MyHeading>
                 <Box
                   display={"grid"}
                   gridTemplateColumns={[
@@ -141,14 +150,20 @@ function MainPage() {
                           return <MainTableItem e={e} key={e.id} />;
                       }) || <Text>Немає жодної карти</Text>}
                 </Box>
-              </TabPanel>
-            ))}
-          </TabPanels>
-        </Box>
-      </Tabs>
-      {/* <Heading textAlign={"center"} mt={"20px"}>
-        Бізнес-плани
-      </Heading>
+              </Box>
+            </TabPanel>
+          ))}
+        </TabPanels>
+      </Box>
+    </Tabs>
+  );
+}
+function BusinessScreen() {
+  const { map, TEJ } = useContext(Context);
+  const navigate = useNavigate();
+  const windW = window.innerWidth;
+  return (
+    <>
       <Tabs
         orientation={windW < 770 ? "horizontal" : "vertical"}
         w={"100vw"}
@@ -163,6 +178,7 @@ function MainPage() {
           mx={"auto"}
           mt={"15px"}
         >
+          <HeroSection />
           <Box
             p={"15px"}
             overflowX={windW < 770 ? "scroll" : undefined}
@@ -176,7 +192,7 @@ function MainPage() {
               >
                 ТЕХНОЛОГІЇ
               </Tab>
-              {/* {TEJ.technologies.map((el) => (
+              {TEJ.technologies.map((el) => (
                 <Tab
                   display={"block"}
                   textAlign={"left"}
@@ -184,33 +200,13 @@ function MainPage() {
                 >
                   {el.name}
                 </Tab>
-              ))} 
+              ))}
             </TabList>
           </Box>
           <TabPanels>
-            {/* <TabPanel p={0}>
-              <Box
-                display={"grid"}
-                gridTemplateColumns={[
-                  "1fr",
-                  "1fr",
-                  "1fr 1fr",
-                  "1fr 1fr",
-                  "1fr 1fr 1fr",
-                ]}
-                gridColumnGap={"15px"}
-                rowGap={"20px"}
-                mx={"auto"}
-              >
-                {map.isLoading
-                  ? [<SkeletonCart />, <SkeletonCart />, <SkeletonCart />]
-                  : TEJ.agreeJustification.map((e) => {
-                      return <MainTableItemJustification e={e} key={e.id} />;
-                    }) || <Text>Немає жодної карти</Text>}
-              </Box>
-            </TabPanel> */}
-      {/* {TEJ.technologies.map((el) => (
-              <TabPanel>
+            <TabPanel p={0}>
+              <Box>
+                <MyHeading mb={4}>Бізнес-плани</MyHeading>
                 <Box
                   display={"grid"}
                   gridTemplateColumns={[
@@ -221,21 +217,60 @@ function MainPage() {
                     "1fr 1fr 1fr",
                   ]}
                   gridColumnGap={"15px"}
+                  rowGap={"20px"}
+                  mx={"auto"}
                 >
                   {map.isLoading
                     ? [<SkeletonCart />, <SkeletonCart />, <SkeletonCart />]
                     : TEJ.agreeJustification.map((e) => {
-                        if (el.id == e.cultivationTechnologyId)
-                          return (
-                            <MainTableItemJustification e={e} key={e.id} />
-                          );
+                        return <MainTableItemJustification e={e} key={e.id} />;
                       }) || <Text>Немає жодної карти</Text>}
                 </Box>
+              </Box>
+            </TabPanel>
+            {TEJ.technologies.map((el) => (
+              <TabPanel>
+                {" "}
+                <Box>
+                  <MyHeading mb={4}>Бізнес-плани</MyHeading>
+                  <Box
+                    display={"grid"}
+                    gridTemplateColumns={[
+                      "1fr",
+                      "1fr",
+                      "1fr 1fr",
+                      "1fr 1fr",
+                      "1fr 1fr 1fr",
+                    ]}
+                    gridColumnGap={"15px"}
+                  >
+                    {map.isLoading
+                      ? [<SkeletonCart />, <SkeletonCart />, <SkeletonCart />]
+                      : TEJ.agreeJustification.map((e) => {
+                          if (el.id == e.cultivationTechnologyId)
+                            return (
+                              <MainTableItemJustification e={e} key={e.id} />
+                            );
+                        }) || <Text>Немає жодної карти</Text>}
+                  </Box>
+                </Box>
               </TabPanel>
-            ))} 
+            ))}
           </TabPanels>
         </Box>
-      </Tabs> */}
+      </Tabs>
+    </>
+  );
+}
+function MainPage() {
+  const { map, TEJ } = useContext(Context);
+  const navigate = useNavigate();
+  const windW = window.innerWidth;
+
+  return (
+    <Box>
+      <BusinessScreen />
+      <MapScreen />
     </Box>
   );
 }
